4e646585ce38bc9ab242caf031f6e4859f4ea36c
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-14 14:54:26 +0300

    Add custom Interface

---
+++ TestServices/Services/IMessageSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestServices.Services
+{
+    interface IMessageSender
+    {
+        string Send();
+    }
+}
